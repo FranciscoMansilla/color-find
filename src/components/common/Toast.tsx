1f5f2a655b78ba81d20ba483a2c07bb4399d7a81
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-07-17 16:08:10 -0300

    refactor Toast

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,9 +1,18 @@
+import { useSelector } from "react-redux"
 
 
 const Toast = ()=>{
+  const {colorPrimary, colorSecondary , colorTertiary } = useSelector((state:any)=>state)
+
   return(
-    <div className="toast">
-      <div>
+    <div className="toast" style={{
+      border: "2px solid "+colorSecondary,
+      color: colorSecondary,
+    }}>
+      <div style={{
+        background: colorTertiary,
+        border: '2px solid ' + colorTertiary,
+      }}>
         <h4>ColorFind Message</h4>
         <p>
           15 mins ago
